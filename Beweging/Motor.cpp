a40c2122f4091eff42d1fdc6f5c3c520e10333f2
LegendaryBosma <64129961+LegendaryBosma@users.noreply.github.com>
2022-05-25 10:34:23 +0200

    Beweging test code

---
+++ Beweging/Motor.cpp
@@ -0,0 +1,22 @@
+﻿#include "Motor.h"
+
+using namespace std;
+
+int main()
+{
+	int pin = 1;
+	int pwmValue = 0;
+
+	wiringPiSetup();
+
+	softPwmCreate(pin, 0, 100);
+
+	for (;;) {
+		std::cout << "Enter snelheid:";
+		std::cin >> pwmValue;
+
+		softPwmWrite(pin, pwmValue);
+		printf("Snelheid nu:", pwmValue);
+
+	}
+}
---
+++ Beweging/Motor.h
@@ -0,0 +1,13 @@
+﻿// CMakeProject1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// TODO: Reference additional headers your program requires here.
