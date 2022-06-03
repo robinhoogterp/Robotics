b69ec99553ac8292b547612a2e67901c42900602
LegendaryBosma <legendarybosma7@gmail.com>
2022-06-03 10:09:16 +0100

    Random throttle

--- Keyboard/Keyboard.cpp
+++ Keyboard/Keyboard.cpp
@@ -2,6 +2,7 @@
 #include "timercpp.h"
 #include <cstdlib>
 
+
 using namespace std;
 
 int main()
@@ -13,16 +14,10 @@ int main()
 	int max = 100;
 	int throttle;
 	int timing;
-
-	//cout << "Vul een getal in om te rijden" << endl;
-	//cout << "0 t/m 2047.5 om achteruit te rijden (0 is maximale snelheid)" << endl;
-	//cout << "2047.5 t/m 4095 om vooruit te rijden (4095 is maximale snelheid)" << endl;
-	//cin >> throttle;
-	
-	if(throttle > 4095)
-	{
-		throttle = 4095;
-	}
+	double scaling;
+	double factor;
+	double forward_thrust;
+	double backward_thrust;
 
 	wiringPiSetup();
 	softPwmCreate(pin, 0, 100);
@@ -30,26 +25,30 @@ int main()
 	pinMode(pin_backwards,OUTPUT);
 	
 	Timer t;
-	
-	double scaling = (throttle / 4095.0) / 0.5 - 1.0; // 2047.5 0VALUE 
-	double factor = scaling * max / 87.4427191;
-	double forward_thrust = factor * scaling * max * 0.01;
-	double backward_thrust = -factor * -scaling * max * 0.01;
+	 t.setInterval([&]() 
+		{
+			if (timing > 30){
+				
+			throttle = rand() % 2047 + 2047;
+			scaling = (throttle / 4095.0) / 0.5 - 1.0; // 2047.5 0VALUE 
+			factor = scaling * max / 87.4427191;
+			forward_thrust = factor * scaling * max * 0.01;
+			backward_thrust = -factor * -scaling * max * 0.01;
+			
+			if(throttle > 4095)
+			{	
+				throttle = 4095;
+			}
+			timing = 0;
+		} else {
+			timing++;	
+			}
+		}, 100); 
 	
 	if(throttle >= 2097.5)
 	{
     t.setInterval([&]() 
     {
-		if (timing > 30){
-			throttle = rand() % 2047 + 2047;
-			scaling = (throttle / 4095.0) / 0.5 - 1.0; // 2047.5 0VALUE 
-			factor = scaling * max / 87.4427191;
-			forward_thrust = factor * scaling * max * 0.01;
-			timing = 0;
-		} else {
-			timing++;	
-			}
-		
 		if(forward_thrust >= scaling * max)
 		{
 			forward_thrust = scaling * max;
@@ -96,7 +95,6 @@ int main()
 	softPwmWrite(pin, 0);
 	        cout << "Brake" << endl;
 		}
-	std::cout << "EIND" << endl;
 	
     while(true); // Keep main thread active
 }
