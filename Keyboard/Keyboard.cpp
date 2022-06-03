8c74fd5c336086628b466ec0b0f6c1f96a68e36c
LegendaryBosma <legendarybosma7@gmail.com>
2022-06-03 11:54:26 +0100

    Finished build up etc

--- Keyboard/Keyboard.cpp
+++ Keyboard/Keyboard.cpp
@@ -12,12 +12,14 @@ int main()
 	int pin_backwards = 28;
 	double timer = 0;
 	int max = 100;
-	int throttle;
 	int timing;
+	int throttle;
 	double scaling;
 	double factor;
 	double forward_thrust;
 	double backward_thrust;
+	double goal_thrust;
+	double current_thrust = 0;
 
 	wiringPiSetup();
 	softPwmCreate(pin, 0, 100);
@@ -25,67 +27,66 @@ int main()
 	pinMode(pin_backwards,OUTPUT);
 	
 	Timer t;
-	 t.setInterval([&]() 
+	t.setInterval([&]() 
 		{
-			if (timing > 30){
-				
-			throttle = rand() % 2047 + 2047;
+			if(timing > 30){
+			throttle = rand() % 4095 + 0;	
+			timing = 0;		
+				} else {
+					timing++;
+					}
+			
 			scaling = (throttle / 4095.0) / 0.5 - 1.0; // 2047.5 0VALUE 
+			goal_thrust = scaling * max;
 			factor = scaling * max / 87.4427191;
-			forward_thrust = factor * scaling * max * 0.01;
-			backward_thrust = -factor * -scaling * max * 0.01;
+			forward_thrust = factor * scaling * max * 1.0;
+			backward_thrust = -factor * -scaling * max * 1.0;
 			
 			if(throttle > 4095)
 			{	
 				throttle = 4095;
 			}
-			timing = 0;
-		} else {
-			timing++;	
-			}
-		}, 100); 
 	
-	if(throttle >= 2097.5)
-	{
-    t.setInterval([&]() 
-    {
-		if(forward_thrust >= scaling * max)
+		if(throttle >= 2097.5)
 		{
-			forward_thrust = scaling * max;
+			if(current_thrust >= scaling * max)
+			{
+				current_thrust = scaling * max;
 		} else {
-			forward_thrust += sqrt(forward_thrust * 0.5);
+			
+			current_thrust += sqrt(forward_thrust * 0.1);
+			forward_thrust = current_thrust;
 		}
 	
     digitalWrite(pin_forward, 1);
 	digitalWrite(pin_backwards, 0);
-	softPwmWrite(pin, forward_thrust);
+	softPwmWrite(pin, current_thrust);
 	timer+=0.1;
 	
 		cout << "Timer: " << " " << timer << " Seconden" << endl;
-		cout << "Snelheid: " << " " << forward_thrust << endl;
+		cout << "Snelheid: " << " " << current_thrust << endl;
 		cout << "Scaling: " << " " << scaling << endl;
 		cout << "Throttle: " << " " << throttle << endl;
 		
-    }, 100); 
 
 	} else if(throttle <= 1997.5)
 	{
-	    t.setInterval([&]() 
-	    {
-		if(backward_thrust >= -scaling * max)
+		if(current_thrust >= -scaling * max)
 		{
-			backward_thrust = -scaling * max;
+			current_thrust = -scaling * max;
 		} else {
-			backward_thrust += sqrt(backward_thrust*0.5);
+			current_thrust += sqrt(backward_thrust*0.1);
+			backward_thrust = current_thrust;
 		}
 	
     digitalWrite(pin_forward, 0);
 	digitalWrite(pin_backwards, 1);
-	softPwmWrite(pin, backward_thrust);
+	softPwmWrite(pin, current_thrust);
 	timer+=0.1;
-		cout << "Timer:" << " " << timer << " Seconden" << endl;
-		cout << "Snelheid: " << " " << backward_thrust << endl;
-    }, 100); 
+		cout << "Timer: " << " " << timer << " Seconden" << endl;
+		cout << "Snelheid: " << " " << current_thrust << endl;
+		cout << "Scaling: " << " " << scaling << endl;
+		cout << "Throttle: " << " " << throttle << endl;
 	} 
 	else 
 	{
@@ -93,8 +94,10 @@ int main()
 	digitalWrite(pin_forward, 0);
 	digitalWrite(pin_backwards, 0);
 	softPwmWrite(pin, 0);
+	current_thrust = 0;
 	        cout << "Brake" << endl;
 		}
+}, 100); 
 	
     while(true); // Keep main thread active
 }
