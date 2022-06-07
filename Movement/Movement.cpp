b21cee4357ef26e34dcf0f6a534e181d578e9ec3
LegendaryBosma <legendarybosma7@gmail.com>
2022-06-07 13:27:29 +0100

    Refactored to class

--- Movement/Movement.cpp
+++ Movement/Movement.cpp
@@ -1,58 +1,27 @@
 #include "Movement.h"
-#include "timercpp.h"
-#include <cstdlib>
-
 
 using namespace std;
 
-int main()
-{
-	int pin = 26;
-	int pin_forward = 27;
-	int pin_backwards = 28;
-	double timer = 0;
-	int max = 100;
-	int temporary;
-	int throttle;
-	double scaling;
-	double factor;
-	double forward_thrust;
-	double backward_thrust;
-	double current_thrust = 0;
-
+void Movement::setup(){
 	wiringPiSetup();
 	softPwmCreate(pin, 0, 100);
 	pinMode(pin_forward,OUTPUT);
 	pinMode(pin_backwards,OUTPUT);
-	
+}
+void Movement::moveDC(double scaling, double factor, double forward_thrust, double backward_thrust , int throttle)
+{
 	Timer t;
 	t.setInterval([&]() //0.1 seconde interval
 		{
-			if(temporary> 30){ 
-			throttle = rand() % 4095 + 0;	
-			temporary = 0;		
-				} else {
-					temporary++;
-					}
-			
-			scaling = (throttle / 4095.0) / 0.5 - 1.0; // Joystick value bruikbaar maken, getal van 0 tot 1
-			factor = scaling * max / 87.4427191; // Scaling kleiner maken, voor een goede versnelling
-			forward_thrust = factor * scaling * max * 1.5; // Forward_thrust bepalen
-			backward_thrust = -factor * -scaling * max * 1.5; // Backward_thrust bepalen
-			
-			if(throttle > 4095)
-			{	
-				throttle = 4095;
-			}
 	
 		if(throttle >= 2097.5)
 		{
-			if(current_thrust >= scaling * max) // Doelsnelheid bereikt
+			if(current_thrust >= scaling * 100) // Doelsnelheid bereikt
 			{
-				current_thrust = scaling * max; // Maximaliseren op doelsnelheid
+				current_thrust = scaling * 100; // Maximaliseren op doelsnelheid
 		} else {
 			
-			current_thrust += sqrt(forward_thrust * 0.1); // Lagere snelheid, hogere value zodat opbouw tijd korter is bij lagere snelheid en hoger bij hogere snelheid. 
+			current_thrust += sqrt(forward_thrust * 0.35); // Lagere snelheid, hogere value zodat opbouw tijd korter is bij lagere snelheid en hoger bij hogere snelheid. 
 			forward_thrust = current_thrust; // Nieuwe snelheid in forward_thrust zetten
 		}
 	
@@ -69,11 +38,11 @@ int main()
 
 	} else if(throttle <= 1997.5)
 	{
-		if(current_thrust >= -scaling * max)
+		if(current_thrust >= -scaling * 100)
 		{
-			current_thrust = -scaling * max;
+			current_thrust = -scaling * 100;
 		} else {
-			current_thrust += sqrt(backward_thrust * 0.1);
+			current_thrust += sqrt(backward_thrust * 0.35);
 			backward_thrust = current_thrust;
 		}
 	
@@ -98,4 +67,26 @@ int main()
 }, 100); 
 	
     while(true); // Keep main thread active
+}
+
+void Movement::receivedata(int input1, int input2){
+	if(input1 > 4095)
+		{	
+			input1 = 4095;
+		}
+	
+	scaling = (input1 / 4095.0) / 0.5 - 1.0; // Joystick value bruikbaar maken, getal van 0 tot 1
+	factor = scaling * 100 / 87.4427191; // Scaling kleiner maken, voor een goede versnelling
+	forward_thrust = factor * scaling * 100 * 1.5; // Forward_thrust bepalen
+	backward_thrust = -factor * -scaling * 100 * 1.5; // Backward_thrust bepalen
+	setup();
+	moveDC(scaling, factor, forward_thrust , backward_thrust , input1);	
+	
+}
+	
+int main()
+{
+	Movement mv;
+	mv.receivedata(4095,3000);
+	return 0;
 }
--- Movement/Movement.h
+++ Movement/Movement.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <wiringPi.h>
+#include "timercpp.h"
+#include <cstdlib>
 #include <softPwm.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -9,3 +11,22 @@
 #include <chrono>
 #include <cstdlib>
 #include <atomic>
+
+class Movement {
+		const int pin = 26;
+		const int pin_forward = 27;
+		const int pin_backwards = 28;
+		double timer = 0;
+		int temporary;
+		int throttle;
+		double scaling;
+		double factor;
+		double forward_thrust;
+		double backward_thrust;
+		double current_thrust = 0;
+		
+		public:
+		void setup();
+		void moveDC(double scaling, double factor, double forward_thrust, double backward_thrust , int throttle);
+		void receivedata(int input1, int input2);
+};
